cf68d79d05dbcb85ca693e6e986aae3257d9dc64
SHS PC <danieljosh090610@gmail.com>
2026-09-02 14:16:02 +0800

    my part sa code

---
+++ app/src/test/java/com/example/myapplication/quarter2/ocampoprofile.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.quarter2;
+import org.junit.Test;
+public class ocampoprofile {
+    @Test
+    public void MyProfile() {
+        // for the input part
+    String myname = "Daniel";
+    String nickname = "Denial";
+    String favgame = "Expedition 33";
+    String ideused = "IntelliJ Idea";
+    int myage = 16;
+    // for the output part
+        System.out.println("Hello! My name is " + myname + " On the internet, I usually go by " + nickname);
+        System.out.println("I enjoy gaming and coding, and I usually use " + favgame + " " + ideused);
+        System.out.println("My birthday is on September 6, and I am soon to be " + myage);
+    }
+}
