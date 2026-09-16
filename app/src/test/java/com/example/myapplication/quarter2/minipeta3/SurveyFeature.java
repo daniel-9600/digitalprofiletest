8a5a868a1a19ef904d67e8cbd032c625e6e4b441
Steve Francis San Andres <sanandresstevefrancis@gmail.com>
2026-09-16 14:33:13 +0800

    moved to a new subclass, minipeta2

--- app/src/test/java/com/example/myapplication/quarter2/minipeta3/SurveyFeature.java
+++ app/src/test/java/com/example/myapplication/quarter2/minipeta3/SurveyFeature.java
@@ -1,4 +1,22 @@
-package com.example.myapplication.quarter2.minipeta3;
+package com.example.myapplication.quarter2.minipeta3; // TODO: Change this to your actual package name
+
+import java.util.Scanner;
 
 public class SurveyFeature {
+
+
+    public void start(Scanner scanner, boolean Survey) {
+        System.out.println("===WELCOME TO THE EXAMPLE SEGMENT===");
+        System.out.println("==What's the capital of france?==");
+
+        int choice = 1;
+        boolean Choosing = true;
+        while (Choosing) {
+            if (choice % 2 == 0) {
+                System.out.println("Answered Yes");
+            } else {
+                System.out.println("Answered No");
+            }
+        }
+    }
 }
