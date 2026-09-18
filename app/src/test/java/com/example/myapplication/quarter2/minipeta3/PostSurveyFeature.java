fd7f72fa61d0c82c39c6c6a8c3985bfcd3cc8a76
Ellah245 <ellahamante3@gmail.com>
2026-09-18 15:08:09 +0800

    tamayos work

--- app/src/test/java/com/example/myapplication/quarter2/minipeta3/PostSurveyFeature.java
+++ app/src/test/java/com/example/myapplication/quarter2/minipeta3/PostSurveyFeature.java
@@ -1,4 +1,20 @@
 package com.example.myapplication.quarter2.minipeta3;
+import org.junit.Test;
+import java.util.Scanner;
 
 public class PostSurveyFeature {
-}
+  public void start(Scanner scanner) {
+    int Scores = 50;
+    boolean isRunning = true;
+
+    while (isRunning) {
+      System.out.println("=============");
+      System.out.println("=======");
+      System.out.println("Top 3 Subjects are example, example, example");
+      System.out.println("Saving Scores");
+      System.out.println("Display the possible outcomes Careers,Pathways,Jobs");
+      System.out.println("=======");
+      System.out.println("============");
+
+    // OPTION 1: WOULD YOU LIKE YOUR SCORES MADE IN PUBLIC?
+  
