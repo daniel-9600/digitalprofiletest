1fb9b864bccbd2657736f7b76d8032acba8ad93e
Ellah245 <ellahamante3@gmail.com>
2026-09-23 15:06:53 +0800

    Final Work of TamayoFastfoodMenu

---
+++ app/src/test/java/com/example/myapplication/quarter2/practicalexam/TamayoFastfoodMenu.java
@@ -0,0 +1,37 @@
+package com.example.myapplication.quarter2.practicalexam;
+import java.util.Scanner;
+
+public class TamayoFastfoodMenu {
+    public static void start(Scanner scanner) {
+        System.out.println("===================");
+        System.out.println("Start Taking Order?");
+        System.out.println("Start Ordering your meal");
+        System.out.println("==================");
+        int picking;
+        int adding;
+        int choice3;
+        int choice4;
+        boolean MenuLoop = true;
+        while (MenuLoop) {
+            picking = scanner.nextInt();
+            adding = scanner.nextInt();
+            choice3 = scanner.nextInt();
+            choice4 = scanner.nextInt();
+            if (picking == 1) {
+                System.out.println("Ordered Burger as Combo");
+            } if (adding == 1) {
+                System.out.println("Ordered Burger as Solo");
+            } if (choice3 == 1) {
+                System.out.println("Ordered Fries");
+            }  if (choice4 == 2) {
+                System.out.println("==================");
+                System.out.println("Succesfully ordered your food");
+                System.out.println("Choose exit");
+                System.out.println("==================");
+                MenuLoop = false;
+            }
+        }
+    }
+}
+
+
