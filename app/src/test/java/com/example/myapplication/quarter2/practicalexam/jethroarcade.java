201f5965bc828c0ca8f5c387f95244952c2233d6
gelics25 <angelicaaquino1008@gmail.com>
2026-09-23 15:07:38 +0800

    poopoo

---
+++ app/src/test/java/com/example/myapplication/quarter2/practicalexam/jethroarcade.java
@@ -0,0 +1,19 @@
+package com.example.myapplication.quarter2.practicalexam;
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+
+public class jethroarcade {
+    public void start(Scanner scanner) {
+        System.out.println("---WELCOME TO JETHRO ARCADE!---");
+
+        int choice = 1;
+
+        if (choice == 1); {
+            System.out.println("you have bought 20 tokens, please enjoy!");
+        }
+        else if (choice == 2) {System.out.println("you have claimed your prize!");
+        }
+    }
+}
