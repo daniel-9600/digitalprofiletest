4fe2c6201b962495dc5a7cbffe09e67d2bd500e2
SHS PC <danieljosh090610@gmail.com>
2026-09-23 14:21:40 +0800

    Fixed Main Menu and all of logic.

--- app/src/test/java/com/example/myapplication/quarter2/practicalexam/OcampoLibraryMenu.java
+++ app/src/test/java/com/example/myapplication/quarter2/practicalexam/OcampoLibraryMenu.java
@@ -2,29 +2,40 @@ package com.example.myapplication.quarter2.practicalexam;
 
 import java.util.Scanner;
 public class OcampoLibraryMenu {
-    public void start (Scanner scanner) {
+    public void start(Scanner scanner) {
         System.out.println("====================");
         System.out.println("Welcome to the Library!");
         System.out.println("Would you like to borrow a book?");
+        System.out.println("====================");
         int choice;
-        int paying;
         int payment;
         boolean MenuLoop = true;
         while (MenuLoop) {
-             choice = scanner.nextInt();
+            choice = scanner.nextInt();
             if (choice == 1) {
                 System.out.println("Example Book Selected!");
-                System.out.println("Fine is = ₱50");
-                choice = scanner.nextInt();
+                System.out.println("Fine is = ₱15");
+                System.out.println("====================");
             } else if (choice == 2) {
                 System.out.println("Paying fine...");
+                System.out.println("====================");
                 payment = scanner.nextInt();
-                System.out.println(payment);
-            } else if (choice == 3){
+                if (payment < 15) {
+                    System.out.println("Insufficient Funds!");
+                    System.out.println("Your payment is = " + payment);
+                    System.out.println("Payment should be ₱15");
+                    System.out.println("====================");
+                } else if (payment >= 15) {
+                    System.out.println("Payment Successful!");
+                    int change = payment - 15;
+                    System.out.println("Change is = " + change);
+                    System.out.println("====================");
+                }
+            } else if (choice == 3) {
+                System.out.println("Exiting.");
+                System.out.println("====================");
                 MenuLoop = false;
-            }
             }
         }
     }
-
-
+}
