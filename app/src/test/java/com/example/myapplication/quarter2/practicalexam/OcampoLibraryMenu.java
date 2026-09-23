892c5686dc1c6f9a09608ee25e1c7c1ee5c2ab50
SHS PC <danieljosh090610@gmail.com>
2026-09-23 14:01:20 +0800

    Saving progress for unfinished menu loop

--- app/src/test/java/com/example/myapplication/quarter2/practicalexam/OcampoLibraryMenu.java
+++ app/src/test/java/com/example/myapplication/quarter2/practicalexam/OcampoLibraryMenu.java
@@ -7,20 +7,19 @@ public class OcampoLibraryMenu {
         System.out.println("Welcome to the Library!");
         System.out.println("Would you like to borrow a book?");
         int choice;
+        int paying;
         int payment;
         boolean MenuLoop = true;
         while (MenuLoop) {
-            choice = scanner.nextInt();
-            payment = scanner.nextInt();
+             choice = scanner.nextInt();
             if (choice == 1) {
                 System.out.println("Example Book Selected!");
                 System.out.println("Fine is = ₱50");
+                choice = scanner.nextInt();
             } else if (choice == 2) {
                 System.out.println("Paying fine...");
-                if (payment < 15) {
-                    System.out.println("Insufficient Funds.");
-                    System.out.println(payment);
-                }
+                payment = scanner.nextInt();
+                System.out.println(payment);
             } else if (choice == 3){
                 MenuLoop = false;
             }
