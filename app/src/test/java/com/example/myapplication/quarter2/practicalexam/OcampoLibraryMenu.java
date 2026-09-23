a817128aedbe07c0a9b25d6c87d86e4850541d70
SHS PC <danieljosh090610@gmail.com>
2026-09-23 13:18:58 +0800

    Very first structure.

---
+++ app/src/test/java/com/example/myapplication/quarter2/practicalexam/OcampoLibraryMenu.java
@@ -0,0 +1,11 @@
+package com.example.myapplication.quarter2.practicalexam;
+import java.util.Scanner;
+public class OcampoLibraryMenu {
+    public void start (Scanner scanner) {
+        System.out.println("====================");
+        System.out.println("Welcome to the Library!");
+        System.out.println("Would you like to borrow a book?");
+        System.out.println("Example Book selected! Fine is: 50₱");
+    }
+
+}
